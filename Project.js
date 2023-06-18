8ade766988b85d42b29f97912a4129a26f165b0e
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2023-06-18 10:51:58 +0530

    Project.js

---
+++ Project.js
@@ -0,0 +1,14 @@
+function getValuesOfLink(){
+    let valuefHref = document.getElementById("link").href;
+    alert(`The value of the href attribute of the link is ${valuefHref}`)
+    let valuefHreflang = document.getElementById("link").hreflang;
+    alert(`The value of the href attribute of the link is ${valuefHreflang}`)
+    let valuefref = document.getElementById("link").rel;
+    alert(`The value of the href attribute of the link is ${valuefref}`)
+    let valuefTarger = document.getElementById("link").target;
+    alert(`The value of the href attribute of the link is ${valuefTarger}`)
+    let valuefType = document.getElementById("link").type;
+    alert(`The value of the href attribute of the link is ${valuefType}`)
+
+
+}
